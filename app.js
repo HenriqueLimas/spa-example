71aa2a70c48e5ba0d3ae44ce44b9da972a5a893d
henrique.limas <henrique.lima@betha.com.br>
2015-10-28 17:35:22 -0200

    core(Server): create a server for spa application

---
+++ app.js
@@ -0,0 +1,49 @@
+/*
+ * app.js - Hello World
+ */
+/*jslint node : true, continue : true,
+devel : true, indent : 2, maxerr : 50,
+newcap : true, nomen : true, plusplus : true,
+regexp : true, sloppy : true, vars : false,
+white : true
+*/
+/*global */
+'use strict';
+
+var http = require('http');
+var express = require('express');
+var server,
+    app;
+
+app = express();
+
+server = http.createServer(app);
+
+// -------- BEGIN SERVER CONFIGURATION --------
+app.configure(() => {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+
+app.configure('development', function() {
+  app.use(express.logger());
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+
+app.get('/', (request, response) => {
+  response.redirect('/spa.html');
+});
+// -------- END SERVER CONFIGURATION ----------
+
+server.listen(3000);
+
+console.log('Listening  on port %d in %s mode', server.address().port, app.settings.env);
